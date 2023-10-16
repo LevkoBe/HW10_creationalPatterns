33c23b17b5cc03331e3a3ef36547755c87a5864f
LevkoBe <lbeniakh@kse.org.ua>
2023-10-16 13:33:52 +0300

    beginning

---
+++ singleton.js
@@ -0,0 +1,13 @@
+class SingleObject {
+  constructor() {
+    if (instance) {
+      return instance;
+    }
+    instance = this;
+    return instance;
+  }
+}
+
+const obj1 = new SingleObject();
+const obj2 = new SingleObject();
+console.log(obj1 === obj2);
