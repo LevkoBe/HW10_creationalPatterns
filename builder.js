38c0fb98750d60cfc480f148e58209ab0bf6c9a4
LevkoBe <lbeniakh@kse.org.ua>
2023-10-16 14:54:26 +0300

    Prototype pattern

--- builder.js
+++ builder.js
@@ -41,8 +41,23 @@ class Builder {
 
 let builder = new Builder();
 let building1 = builder.buildAHouse(3, 5, 4);
-let building2 = builder.buildAHouse(6, 5, 2);
+let building2 = builder.buildAHouse(5, 5, 2);
 let building3 = builder.buildAHouse(6, 4, 7);
 console.log(`First building has ${building1.floors} floors, and there live ${building1.people} people.`);
 console.log(`Second building has ${building2.floors} floors, and there live ${building2.people} people.`);
 console.log(`Third building has ${building3.floors} floors, and there live ${building3.people} people.`);
+
+class ProtoBuilding {
+  constructor(proto) {
+    let newBuilding = new Building();
+    newBuilding.floors = proto.floors;
+    newBuilding.land = proto.land;
+    newBuilding.people = proto.people;
+    newBuilding.windows = proto.windows;
+    newBuilding.ceiling = proto.ceiling;
+    return newBuilding;
+  }
+}
+
+let building4 = new ProtoBuilding(building2);
+console.log(`Fourth building has ${building4.floors} floors, and there live ${building4.people} people.`);
